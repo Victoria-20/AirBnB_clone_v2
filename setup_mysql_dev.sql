79de1125e04b662f57c945d6114d487199ba0cc3
Victoria-20 <victoriaocon111@gmail.com>
2022-10-10 11:29:46 +0300

    Initial commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Create databases, user and grant privileges
+-- Create dev database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
